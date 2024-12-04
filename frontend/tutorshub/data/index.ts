0f3bfb2454940f178d8f5c113e875ef8d6bdd298
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-12-04 18:03:45 +0300

    changed images

--- frontend/tutorshub/data/index.ts
+++ frontend/tutorshub/data/index.ts
@@ -220,8 +220,8 @@ export const person = {
         return `${this.firstName} ${this.lastName}`;
     },
     role:      'Professional Tutor',
-    avatar:    '/maths.svg',
-    location:  'Nairobi/Kenya',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+    avatar:    '/pfp.jpg',
+    location:  'Globall',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
     languages: ['English', 'Swahili']  // optional: Leave the array empty if you don't want to display languages
 }
 
